b244f389e3ed84e0ad26daba68e64e9f61ce518a
Ade Balogun <abalogun316@gmail.com>
2021-08-13 02:59:48 +0100

    portal for bulk user actions from admin

---
+++ src/components/pages/CommandPortal.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const CommandPortal = () => {
+  return (
+    <main>
+      <div className="container-md my-5">
+        <div className="row">
+          <div className="col-sm-1"></div>
+          <div className="col-sm-10">
+            <div className="row">
+              <h1>Command Portal</h1>
+              <p>Bulk comms and actions for users who meet prescribed conditions.</p>
+              <hr />
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <div class="card">
+                  <div class="card-body">
+                    This is some text within a card body.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-1"></div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default CommandPortal;
